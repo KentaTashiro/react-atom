b9667233fc83f0f36485f296a9794a36e8f1927e
KentaTashiro <62204430+KentaTashiro@users.noreply.github.com>
2022-01-08 17:43:33 +0900

    Organismの作成

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
 
 export default function App() {
+  const user = {
+    name: "たしろ～～",
+    image: "https://source.unsplash.com/TzjMd7i5WQI",
+    email: "tashiro.co.jp",
+    phone: "111-2222-3333",
+    company: {
+      name: "ココロザシ"
+    },
+    website: "https://www.youtube.com/"
+  };
   return (
     <div className="App">
       <PrimaryButton>Primary</PrimaryButton>
       <SecondaryButton>Secondary</SecondaryButton>
       <br />
       <SearchInput></SearchInput>
+      <br />
+      <UserCard user={user} />
     </div>
   );
 }
